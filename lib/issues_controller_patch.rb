7dbd340b61418015516dce44020044e2db4ef99b
kiioong <kiioong94@gmail.com>
2014-08-20 02:49:02 -0700

    changed condition so the chosen projects should now work correctly

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -17,10 +17,10 @@ module RedmineSpentTimeRequired
         def update_with_check_spent_time
           allowed_projects = Setting.plugin_redmine_spent_time_required['projects'].scan(/\w+/)
           allowed_statuses = Setting.plugin_redmine_spent_time_required['statuses'].scan(/\d+/)
-          current_project = Project.find(params[:issue][:project_id])
-          current_status = params[:issue][:status_id]
+          current_project = Project.find(params[:issue][:project_id]) #Issue.find(params[:issue]).project
+          current_status = params[:issue][:status_id] 
           if ((!params[:time_entry].nil?) && (!Issue.find(params[:id]).children?))
-            if ((params[:time_entry][:hours] == "") && (allowed_projects.member?(current_project.to_param)) && (allowed_statuses.member?(current_status.to_s)))
+            if ((params[:time_entry][:hours] == "") && (allowed_projects.member?(current_project.id.to_s)) && (allowed_statuses.member?(current_status.to_s)))
               find_issue
               update_issue_from_params
               @time_entry.errors.add :hours, :empty
